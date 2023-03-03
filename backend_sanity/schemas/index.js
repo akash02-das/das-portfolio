af30466826219629ef09cc8bc6490c738e7bfd96
Akash Das <aku.das02@gmail.com>
2023-03-03 23:52:06 +0600

    setup back-end with sanity

---
+++ backend_sanity/schemas/index.js
@@ -0,0 +1,8 @@
+import about from './about'
+import contact from './contact'
+import experiences from './experiences'
+import skills from './skills'
+import workExperience from './workExperience'
+import works from './works'
+
+export const schemaTypes = [about, experiences, workExperience, works, skills, contact]
