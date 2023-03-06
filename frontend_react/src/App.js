8a2891814d9caf184106b031a1abc972f804e8ac
Akash Das <aku.das02@gmail.com>
2023-03-06 22:40:14 +0600

    style and responsive work section

--- frontend_react/src/App.js
+++ frontend_react/src/App.js
@@ -10,9 +10,9 @@ const App = () => {
       <Navbar />
       <Header />
       <About />
-      <Footer />
-      <Skills />
       <Work />
+      <Skills />
+      <Footer />
     </div>
   );
 };
