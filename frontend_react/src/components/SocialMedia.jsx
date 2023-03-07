7f80c83ad8ea4774fd4d52776efddc0ab7f91710
Akash Das <aku.das02@gmail.com>
2023-03-08 00:04:52 +0600

    add social media links

--- frontend_react/src/components/SocialMedia.jsx
+++ frontend_react/src/components/SocialMedia.jsx
@@ -4,15 +4,29 @@ import { BsFacebook, BsGithub, BsLinkedin } from "react-icons/bs";
 const SocialMedia = () => {
   return (
     <div className="app__social">
-      <div>
-        <BsLinkedin />
-      </div>
-      <div>
-        <BsGithub />
-      </div>
-      <div>
-        <BsFacebook />
-      </div>
+      <a
+        href="https://www.linkedin.com/in/akash-das02"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <div>
+          <BsLinkedin />
+        </div>
+      </a>
+      <a href="https://github.com/akash02-das" target="_blank" rel="noreferrer">
+        <div>
+          <BsGithub />
+        </div>
+      </a>
+      <a
+        href="https://www.facebook.com/akash.rainboy"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <div>
+          <BsFacebook />
+        </div>
+      </a>
     </div>
   );
 };
